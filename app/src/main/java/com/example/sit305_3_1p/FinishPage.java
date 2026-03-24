f9ba21338fc6a8ce6e58a7d71ed34a7605e05aae
jezzif <jezzif12@gmail.com>
2026-03-24 22:23:26 +1100

    Shows user amount of correct answers on FinishPage

--- app/src/main/java/com/example/sit305_3_1p/FinishPage.java
+++ app/src/main/java/com/example/sit305_3_1p/FinishPage.java
@@ -1,6 +1,8 @@
 package com.example.sit305_3_1p;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,13 @@ public class FinishPage extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        SharedPreferences sharedPreferences = getSharedPreferences("com.example.sit305_3_1p", MODE_PRIVATE);
+        int correctly = sharedPreferences.getInt("correctly", 0);
+        String yeah = "You got " + correctly + " correct!";
+
+        TextView correctText = findViewById(R.id.correctText);
+
+        correctText.setText(yeah);
     }
 }
